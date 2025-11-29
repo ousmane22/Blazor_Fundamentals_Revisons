a63296c19211df49868db5a662f2d2fe831316ad
ousmane <ousmane4@live.fr>
2025-11-29 11:59:51 +0000

    feat:working with parameter

---
+++ ShopHRM/Components/EmployeeCard.razor.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Components;
+using ShopHRM.Shared.Domain;
+
+
+namespace ShopHRM.Components
+{
+    public partial class EmployeeCard
+    {
+        [Parameter]
+        public Employee  Employee { get; set; } = new Employee();
+    }
+}
